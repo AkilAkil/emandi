01276b55cfaf05d4f8fed4312fe573ea23c6328f
AkilAkil <agilan.madhiyan@gmail.com>
2017-09-28 17:26:41 +0530

    init

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(function() {
+
+        $('.button-collapse').sideNav();
+        $('.carousel.carousel-slider').carousel({ fullWidth: true });
+
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+$(document).ready(function() {
+    $('.carousel').carousel();
+});
